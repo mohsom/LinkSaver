ec7c7e5d07274599fb11df8d4bad8887e1b5a548
mohsom <volodya.skalskyi@gmail.com>
2014-11-14 20:40:42 +0200

    Views.

--- js/views/linkView.js
+++ js/views/linkView.js
@@ -1,3 +1,19 @@
 /**
  * Created by mohsom on 14.11.2014.
  */
+define([
+    "jquery",
+    "underscore",
+    "backbone"
+],function($,_,Backbone)
+{
+    var LinkView=Backbone.View.extend({
+        tagName:"div",
+        render:function()
+        {
+            var temp=$("#link-template").html();
+            $("#collection").append(temp(this.model.toJSON()));
+        }
+    });
+    return LinkView;
+});
