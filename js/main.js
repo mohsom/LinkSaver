dc38fa1b83e3058fb58f2eb437a35d7a5a0cc539
mohsom <volodya.skalskyi@gmail.com>
2014-11-14 21:17:10 +0200

    Problems with Require.

--- js/main.js
+++ js/main.js
@@ -2,18 +2,17 @@
  * Created by mohsom on 14.11.2014.
  */
 require.config({
-    baseUrl:"../",
-    paths:{
-        jquery:"lib/jquery/jquery-2.1.1.min",
-        underscore:"lib/underscore/underscore",
-        backbone:"lib/backboneJS/backbone",
-        local:"lib/backboneJS/local",
-        text:"lib/require/text"
+    baseUrl: "../",
+    paths: {
+        jquery: "lib/jquery/jquery-2.1.1.min",
+        underscore: "lib/underscore/underscore",
+        backbone: "lib/backboneJS/backbone",
+        local: "lib/backboneJS/local",
+        text: "lib/require/text"
     }
 });
-define([
+require([
     "views/app"
-],function(app){
-    var app_view=new app;
-    new app_view;
+], function (app) {
+    var app_view = new app;
 });
