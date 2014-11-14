c1e103e17c11d18e3af6457ca0044d2f7763b499
mohsom <volodya.skalskyi@gmail.com>
2014-11-14 21:25:29 +0200

    Problems with Require.

--- js/views/linkView.js
+++ js/views/linkView.js
@@ -10,11 +10,19 @@ define([
     var LinkView=Backbone.View.extend({
         tagName:"div",
         className:"link-item",
+        events:{
+          "click .butl":"deleteL"
+        },
         render:function()
         {
             var temp=$("#link-template").html();
             this.$el.html(temp(this.model.toJSON()));
             return this;
+        },
+        deleteL:function()
+        {
+            this.model.destroy();
+            this.remove();
         }
     });
 });
