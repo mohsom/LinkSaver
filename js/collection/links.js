de2b681fc1051fd93829ca1e327dd1bb964a7ca0
mohsom <volodya.skalskyi@gmail.com>
2014-11-14 20:32:18 +0200

    Collections.

--- js/collection/links.js
+++ js/collection/links.js
@@ -1,3 +1,14 @@
 /**
  * Created by mohsom on 14.11.2014.
  */
+define([
+    "backbone",
+    "local",
+    "models/link"
+],function(Backbone,local,LinkModel)
+{
+    var Links=Backbone.Collection.extend({
+        model:LinkModel,
+        local:new Backbone.local("links")
+    });
+});
