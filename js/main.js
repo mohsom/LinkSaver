976e9324581f1baefafb7b1eda662da041ea6e42
mohsom <volodya.skalskyi@gmail.com>
2014-11-14 17:25:26 +0200

    Require.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 /**
  * Created by mohsom on 14.11.2014.
  */
+require.config({
+    baseUrl:"../",
+    path:{
+        jquery:"lib/jquery/jquery-2.1.1.min",
+        underscore:"lib/underscore/underscore",
+        backbone:"lib/backbone/backbone",
+        text:"lib/require/text"
+    }
+});
+require(["views/app"],function(app){
+    var app_view=new app;
+});
