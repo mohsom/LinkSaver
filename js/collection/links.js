721d49e0fa01958ffe9e53f13e29192be9dd1817
mohsom <volodya.skalskyi@gmail.com>
2014-11-15 13:33:45 +0200

    local storage don't works.

--- js/collection/links.js
+++ js/collection/links.js
@@ -1,7 +1,7 @@
 var app = app || {};
 app.Links = Backbone.Collection.extend({
     model: app.LinkModel,
-    localStorage:new Backbone.LocalStorage("links")
+    localStorage:new Backbone.LocalStorage("links") //not works
 });
 console.log("collection init");
 app.links=new app.Links();
