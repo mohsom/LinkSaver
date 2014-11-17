4059d87b3dec6c3c62963da96446f5d5679702f9
mohsom <volodya.skalskyi@gmail.com>
2014-11-17 20:09:48 +0200

    Small fixes.

--- js/collection/links.js
+++ js/collection/links.js
@@ -1,7 +1,7 @@
 var app = app || {};
 var Links = Backbone.Collection.extend({
     model: app.LinkModel,
-    localStorage: new Backbone.LocalStorage("links")
+    localStorage: new Backbone.LocalStorage("links-app")
 });
 console.log("collection init");
 app.links = new Links();
--- release/js/main.js
+++ release/js/main.js
@@ -1438,7 +1438,7 @@ console.log("model init ");
 var app = app || {};
 var Links = Backbone.Collection.extend({
     model: app.LinkModel,
-    localStorage: new Backbone.LocalStorage("links")
+  localStorage: new Backbone.LocalStorage("links-app")
 });
 console.log("collection init");
 app.links = new Links();
