1d527e0102553a85c14d2f465e54c78a4dff40c8
mohsom <volodya.skalskyi@gmail.com>
2014-11-17 11:55:16 +0200

    Small fixes.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,18 @@
 /**
  * Created by mohsom on 17.11.2014.
  */
+module.exports=function(grunt){
+    grunt.initConfig({
+        pkg:grunt.file.readJSON("package.json"),
+        jshint:{
+            options:{
+                curly:true,
+                eqeqeq:true,
+                immned:true,
+                latedef:true,
+                rewcap:true,
+                
+            }
+        }
+    });
+};
