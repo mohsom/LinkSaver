cc9204ab46a0ef78da106fef5b2cd6a4ad30ab88
mohsom <volodya.skalskyi@gmail.com>
2014-11-19 14:41:36 +0200

    Require.

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by mohsom on 19.11.2014.
+ */
+require.config({
+    path:{
+        "jquery":"js/lib/jquery/jquery.min",
+        "underscore":"js/underscore/underscore",
+        "backbone":"js/lib/backbone/backbone-min"
+    }
+});
